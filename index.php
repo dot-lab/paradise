ff264b137e0bc119538d7f8418d0d721786ab624
Yoshie Takeda <info@dot-lab.xyz>
2018-12-03 07:25:23 +0900

    サムネイルを出した

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php get_header(); ?>
-		<div id="main">
-			<article>
-				<h1>Works</h1>
-				<h2>Coloring</h2>
-				<?php 
-					$args = array(
-						'category_name' => 'coloring'
-					);
-					$posts = get_posts($args);
-				?>
-				<ul>
-					<?php foreach($posts as $post) : setup_postdata($post);?>
-						<li>
-							<?php echo the_title();?>
-						</li>
-					<?php endforeach; wp_reset_postdata(); ?>
-				</ul>
-			</article>
-		</div>
-		<?php get_sidebar(); ?>
-		<?php get_footer(); ?>
+    <div id="main">
+      <article>
+        <h1>Works</h1>
+        <h2>Coloring</h2>
+        <?php $args = array(
+          'category_name' => 'coloring'
+        );
+          $query = new WP_Query($args); ?>
+        <?php if($query->have_posts()) : ?>
+            <div class="coloring">
+            <?php while( $query->have_posts() ) : $query->the_post(); ?>
+              <div class="post">
+                <?php $image = get_field('coloring_image'); ?>
+                <img src="<?php echo $image['sizes']['thumbnail'];?>" alt="<?php get_field('coloring_title');?>">
+              </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+            </div>
+        <?php else :?>
+          <p>投稿がみつかりません。</p>
+        <?php endif;?>
+      </article>
+    </div>
+    <?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
-		<aside>
-			<div id="sidebar">
-				<?php $page = get_post(5);?>
-				<h1><?php echo $page -> post_title;?></h1>
-				<?php echo $page -> post_content; ?>
-			</div>
-		</aside>
+    <aside>
+      <div id="sidebar">
+        <?php $page = get_post(5);?>
+        <h1><?php echo $page -> post_title;?></h1>
+        <?php echo $page -> post_content; ?>
+      </div>
+    </aside>
